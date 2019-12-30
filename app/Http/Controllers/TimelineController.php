42b3edd7657e35e70a006a46b376945b7e410f70
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2019-12-30 21:22:27 +0200

    1+2 make Controller

---
+++ app/Http/Controllers/TimelineController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TimelineController extends Controller
+{
+    public function index(){
+        //return __METHOD__;
+        return view('timeline.index');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+//Route::get('/welcome/to/laravel', function () {
+//    return 'Welcome';
+//});
+
+//Route::get('/welcome/{name}', function () {
+//    return 'Welcome!';
+//});
+
+
+//Route::get('/welcome/{name}', function ($name) {
+//    return 'Welcome!' . $name;
+//});
+
+//Route::get('/welcome/{name}/{age?}', function ($name,$age =10) {
+//    return 'Welcome!' . $name .$age;
+//});
+
+
+//Route::get('/welcome/{name}/{age?}', function ($name,$age =10) {
+//    echo config('app.timezone');
+//    return 'Welcome!' . $name .$age;
+//});
+
+
+Route::get('/timeline','TimelineController@index');

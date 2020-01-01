1f3adf8460a839bb8660652cf4d28c0cdfaf710f
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-01-01 23:53:17 +0200

    (إنشاء الموديل وتجهيزهم 3  )

---
+++ app/Post.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    //هذا الوضع الافتراضي للموديل حتى يتعامل مع الجدول الخاص به وهو جدول ال posts
+//    protected $table = 'posts';
+//    protected $primaryKey= 'id';
+//    public $incrementing ='true';
+//    public $timestamps ='true';
+}
